e3024de0ce0823c5c25daaa84010ae39fef81c41
Michael Neis <michael.neis12@gmail.com>
2021-12-14 23:21:08 -0600

    commit tree f85d27365ab5be8c6943606297603b7a62f5e4c9

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,46 @@
-# your code here
+require 'pry'
+
+class MySet
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each{|val| @hash[val] = true}
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true
+        self
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+
+    def size
+        @hash.size
+    end
+
+    def self.[](*enumerable)
+        newSet = self.new(enumerable)
+    end
+
+    def clear
+        @hash = {}
+        self
+    end
+
+    def each(&b)
+        @hash.keys.each{|k| b.call(k)}
+        self
+    end
+    
+    def inspect
+        "#<MySet: {#{@hash.keys.join(', ')}}>"
+    end
+end
+
+# binding.pry
